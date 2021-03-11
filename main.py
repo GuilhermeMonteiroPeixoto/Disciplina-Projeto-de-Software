4a3ff9d5276b9dc21b70974557f2d0550440db1d
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-03-11 19:18:25 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 #!/usr/bin/env python
 # coding: utf-8
+
+from funcionario import CadFuncionario
+import pandas as pd
+
+#Teste
+sis = CadFuncionario()
+sis.tabela_funcionarios()
+sis.inserir_tabela()
+sis.inserir_tabela()
+sis.inserir_tabela()
+
+print(sis.tabela)
