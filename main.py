c4c5f4d069e088435b3e9af91f3236c4de0c66d5
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-03-11 20:16:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -6,9 +6,9 @@ import pandas as pd
 
 #Teste
 sis = CadFuncionario()
-sis.tabela_funcionarios()
-sis.inserir_tabela()
-sis.inserir_tabela()
-sis.inserir_tabela()
-
+sis.criar_tabela_de_funcionarios()
+sis.inserir_funcionario_na_tabela()
+sis.inserir_funcionario_na_tabela()
+sis.remover_funcionario_da_tabela(0)
+sis.modificar_dados_da_tabela(1, 'endereco', 'Salvador')
 print(sis.tabela)
