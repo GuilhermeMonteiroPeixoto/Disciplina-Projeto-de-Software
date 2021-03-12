5cc870b37737067e5b12c26b3dfe3d8716adcad7
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-03-12 16:23:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,12 @@
 
 from funcionario import CadFuncionario
 import pandas as pd
+import numpy as np
 
 #Teste
 sis = CadFuncionario()
 sis.criar_tabela_de_funcionarios()
 sis.inserir_funcionario_na_tabela()
-sis.inserir_funcionario_na_tabela()
-sis.remover_funcionario_da_tabela(0)
+sis.remover_funcionario_da_tabela(1)
 sis.modificar_dados_da_tabela(1, 'endereco', 'Salvador')
 print(sis.tabela)
