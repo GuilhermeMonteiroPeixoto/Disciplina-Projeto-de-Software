dc6de04645d09a9e26e65eded078f9f4888d5542
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-03-23 17:14:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from funcionario import CadFuncionario
-import pandas as pd
-import numpy as np
+from SistemaCadastro import SistemaCadastro
+from Empregado import Empregado
 
-#Teste
-sis = CadFuncionario()
-sis.criar_tabela_de_funcionarios()
-sis.inserir_funcionario_na_tabela()
-sis.remover_funcionario_da_tabela(1)
-sis.modificar_dados_da_tabela(1, 'endereco', 'Salvador')
-print(sis.tabela)
+bd = SistemaCadastro()
+
+CadFuncionario = Empregado()
+
+bd.inserir_funcionario_na_tabela(CadFuncionario)
+
+bd.remover_funcionario_da_tabela(0)
+bd.modificar_dados_da_tabela(0, 'endereco', 'Rua Salvador')
+
+print(bd.tabela)
