f44faa95f51542fe6e2b82074dc46d7b41f553e4
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-03-24 02:54:09 -0300

    Update Empregado.py

--- Empregado.py
+++ Empregado.py
@@ -8,13 +8,24 @@ class Empregado:
         dicionario['nome'] = str(input('Nome: '))
         dicionario['endereco'] = str(input('Endereço: '))
         dicionario['metodo'] = str(input('metodo: '))
+        
+        if dicionario['metodo'] == '2':
+            dicionario['Conta'] = str(input('Conta do Banco:'))
+        
         dicionario['tipo'] = str(input('tipo: '))
+        
+        if dicionario['tipo'] == '0':
+            dicionario['salarioHora'] = str(input('salarioHora: '))
+        elif dicionario['tipo'] == '1':
+            dicionario['salario'] = str(input('salario: '))
+        elif dicionario['tipo'] == '2':
+            dicionario['salarioFixo'] = str(input('salarioFixo: '))
+            
         dicionario['sindicalista'] = str(input('sindicalista: '))
-        dicionario['salarioHora'] = str(input('salarioHora: '))
-        dicionario['salarioFixo'] = str(input('salarioFixo: '))
-        dicionario['percentual'] = str(input('percentual: '))
-        dicionario['salario'] = str(input('salario: '))
-        dicionario['taxaFixa'] = str(input('taxaFixa: '))
+        
+        if dicionario['sindicalista'] == 'S':
+            dicionario['taxaFixa'] = str(input('taxaFixa: '))
+            
         self.informacao = pd.Series(dicionario)
 
 #Para usar essa Class:
