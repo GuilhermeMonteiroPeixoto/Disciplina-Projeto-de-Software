822a070c46c3f90a24bb27e6cc4531c9c1de753b
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-04-01 20:29:49 -0300

    Create CartaoPonto.py

---
+++ CartaoPonto.py
@@ -0,0 +1,7 @@
+class CartaoPonto:
+
+    def __init__(self, timeIn, timeOut, numHoras, numCartao):
+        self._timeIn = timeIn
+        self._timeOut = timeOut
+        self._numHoras = numHoras
+        self._numCartao = numCartao
