95e9573802a61894b23d2b5fcad51b88613a86d1
Guilherme Monteiro Peixoto <53547474+GuilhermeMonteiroPeixoto@users.noreply.github.com>
2021-04-29 01:12:22 -0300

    Edit site_header

--- Usando_DjangoAdmin/crudex/admin.py
+++ Usando_DjangoAdmin/crudex/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
+from django.contrib.auth.models import Group
 from crudex.models import Empregado
 
+admin.site.site_header = 'Folha de Pagamento'
 admin.site.register(Empregado)
+admin.site.unregister(Group)
